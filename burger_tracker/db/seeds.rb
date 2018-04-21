61487e2a3e9bf002a03646c7a6fc0ff10dbde2fb
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-21 14:36:52 +0100

    seed data added for burgers table

---
+++ burger_tracker/db/seeds.rb
@@ -0,0 +1,26 @@
+require_relative("../models/burger.rb")
+require("pry-byebug")
+
+
+ burger1 = Burger.new({
+  "name" => "Cheeseburger"
+  })
+
+  burger1.save()
+
+ burger2 = Burger.new({
+   "name" => "Chicken burger"
+  })
+
+  burger2.save()
+
+ burger3 = Burger.new({
+  "name" => "Veggie burger"
+  })
+
+  burger3.save()
+
+
+
+  binding.pry
+  nil
