048b7a4e3fd816888aba7df3fa9ab656bebb754a
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-22 15:58:12 +0100

    added view to show all burgers

---
+++ burger_tracker/controllers/burgers_controller.rb
@@ -0,0 +1,10 @@
+# require("sinatra")
+# require("sinatra/contrib/all")
+#
+# require_relative("../models/burger.rb")
+#
+#
+# get "/burger-tracker" do
+#   @burgers = Burger.all()
+#   erb(:index)
+# end
