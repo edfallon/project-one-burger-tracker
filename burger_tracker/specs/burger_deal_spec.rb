f340dce9581bb8e9364b357b41bf801029f517cb
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-23 16:57:32 +0100

    passing test for burger_deal_id

--- burger_tracker/specs/burger_deal_spec.rb
+++ burger_tracker/specs/burger_deal_spec.rb
@@ -1,5 +1,5 @@
 require('minitest/autorun')
-require_relative('../models/burger_deal_spec.rb')
+require_relative('../models/burger_deal.rb')
 
 class TestBurgerDeal < MiniTest::Test
 
@@ -7,3 +7,9 @@ class TestBurgerDeal < MiniTest::Test
   def setup
     @burger_deal1 = BurgerDeal.new({"id" => "1", "restaurant_id" => "25", "burger_id" => "7"})
   end
+
+  def test_burger_deal_id
+    assert_equal(1, @burger_deal1.id)
+  end
+
+end
